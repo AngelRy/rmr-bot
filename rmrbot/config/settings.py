531fbc0ddaf17da64da5cfaf6ff408bda1631e7d
AngelRy <angel.dzekov@gmail.com>
2026-03-12 06:33:10 +0100

    Move API tokens to environment variables

--- rmrbot/config/settings.py
+++ rmrbot/config/settings.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 OUTPUT_DIR = os.path.join(BASE_DIR, "output")
@@ -21,9 +24,8 @@ RUNNING_HASHTAGS = [
 ]
 
 
-FB_PAGE_ID = os.getenv("112942243947994")
-FB_PAGE_TOKEN = os.getenv("EAAZB2XLjSxRgBQaryI1Yd0T4O98CZA90kQMJvdSUfoSXPGZCAtl37Sq8dgqXwGGetZCcvUZBdonZC1ZCEErvg5LAKP5f78XqvRvyaCodZBZA5ZCKvSs8GC9nr2pSMLKB2ukZAqV6ZBKTfhZCPrxxsFqP9xaXiwQGU9aZAQ0D3lGz831ZAoW7nnE1rK1fGSBWcp2fQZDZD")
-
+FB_PAGE_ID = os.getenv("FB_PAGE_ID")
+FB_PAGE_TOKEN = os.getenv("FB_PAGE_TOKEN")
 FB_API_BASE = "https://graph.facebook.com/v19.0"
 
 IMAGE_OUTPUT_DIR = "output/images"
